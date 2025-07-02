c4da9ae3dfcd825d681eb26b38230aad2fc7a53c
bezmari19 <marina.bezerraf19@gmail.com>
2025-07-02 13:02:08 -0300

    :books:docs: exercicio novo, comentado e colocado em pasta

---
+++ exercícios resolvidos/conta_básica.c
@@ -0,0 +1,17 @@
+/*Programa básico para somar dois números*/
+#include <stdio.h>
+
+int main()
+{
+    int A; /*declara a variável A*/
+    int B; /*declara a variável B*/
+    scanf("%d", &A); /*lê o valor da variável A que o usuário irá digitar*/
+    scanf("%d", &B); /*lê o valor da variável B que o usuário irá digitar*/
+
+    int soma = A + B; /*declara a variável que armazena os números digitados e realiza a soma pedida no exercício*/
+
+    printf("X = %d", soma); /*imprime o resultado da soma*/
+
+return 0; /*indica que o programa foi executado com sucesso*/
+
+}
